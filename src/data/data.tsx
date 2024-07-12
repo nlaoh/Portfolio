e3685679327e5d98e9df1ee8f2314a2e458465b7
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2024-07-12 15:51:35 +1000

    -changed hero opacity -changed header opacity -changed about description to allow for HTML/JSX formatting

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -110,9 +110,54 @@ export const heroData: Hero = {
  */
 export const aboutData: About = {
   profileImageSrc: profilepic,
-  description: `Use this bio section as your way of describing yourself and saying what you do, what technologies you like
-  to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
-  in.`,
+  description: (
+    <>
+      <p>
+        Hey! I'm Natch and I'm a proactive and dedicated undergraduate student in Computer Science with strong
+        programming skills and hands-on experience in various environments. I seek to leverage my problem-solving
+        abilities and technical knowledge in a dynamic software engineering role.
+      </p>
+      <p>I am well-versed in the following skills:</p>
+      <ol>
+        <li>- Programming Languages: Python, Java, C++</li>
+        <li>- Scripting: Shell scripting - Operating Systems: Linux (Ubuntu)</li>
+        <li>- Tools & Technologies: Git, Docker, SQL</li>
+        <li>- Networking: Basic understanding of TCP/IP, DNS, DHCP</li>
+        <li>- Databases: MySQL, PostgreSQL</li>
+        <li>…Among many others!</li>
+      </ol>
+      <p>
+        I have had experience in various personal projects, that are outlined in my CV; but here are a few of my
+        proudest projects:
+      </p>
+      <ol>
+        <li>- VX User research with Tourism North East (TNE)</li>
+        <li>- Mock full-stack e-commerce application for fresh produce</li>
+        <li>- A website that displays climate change data sourced from the Australian Bureau of Meteorology</li>
+        <li>- Volunteering in the development of the revised IOTAA 2023 website.</li>
+      </ol>
+      <p>
+        I've developed various fundamental skills for programming from my lifelong journey in education. I’ve always
+        loved solving problems and developing solutions that feel satisfying.
+      </p>
+    </>
+  ),
+  //   description: `Hey! I'm Natch and I'm a proactive and dedicated undergraduate student in Computer Science with strong programming skills and hands-on experience in various environments. I seek to leverage my problem-solving abilities and technical knowledge in a dynamic software engineering role.
+  // I am well-versed in the following skills:
+  // - Programming Languages: Python, Java, C++
+  // - Scripting: Shell scripting - Operating Systems: Linux (Ubuntu)
+  // - Tools & Technologies: Git, Docker, SQL
+  // - Networking: Basic understanding of TCP/IP, DNS, DHCP
+  // - Databases: MySQL, PostgreSQL
+
+  // …Among many others!
+
+  // I have had experience in various personal projects, that are outlined in my CV; but here are a few of my proudest projects:
+  // - VX User research with Tourism North East (TNE)
+  // - A website that displays climate change data sourced from the Australian Bureau of Meteorology
+  // - Volunteering in the development of the revised IOTAA 2023 website.
+
+  // I've developed various fundamental skills for programming from my lifelong journey in education. I’ve always loved solving problems and developing solutions that feel satisfying.`,
   aboutItems: [
     {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
     {label: 'Age', text: '29', Icon: CalendarIcon},
