533e793b3c47fc6664c2ea9b9fb5de74a4662112
Natchanon Laoharawee <s4006840@student.rmit.edu.au>
2024-07-19 12:16:09 +1000

    -fixed hero image flickering on page loading

--- src/components/Sections/Hero.tsx
+++ src/components/Sections/Hero.tsx
@@ -1,25 +1,29 @@
-import {ChevronDownIcon} from '@heroicons/react/24/outline';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
 import classNames from 'classnames';
 import Image from 'next/image';
-import {FC, memo} from 'react';
+import { FC, memo } from 'react';
 
-import {heroData, SectionId} from '../../data/data';
+import { heroData, SectionId } from '../../data/data';
 import Section from '../Layout/Section';
 import Socials from '../Socials';
 
 const Hero: FC = memo(() => {
-  const {imageSrc, secondaryName, name, description, actions} = heroData;
+  const { imageSrc, secondaryName, name, description, actions } = heroData;
 
   return (
     <Section noPadding sectionId={SectionId.Hero}>
       <div className="relative flex h-screen w-full items-center justify-center">
-        <Image
-          alt={`${name}-image`}
-          className="absolute z-0 h-full w-full object-cover"
-          placeholder="blur"
-          priority
-          src={imageSrc}
-        />
+        <div className="absolute z-0 h-full w-full">
+          <Image
+            alt={`${name}-image`}
+            className="h-full w-full object-cover transition-opacity duration-500"
+            src={imageSrc}
+            placeholder="blur"
+            priority
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
         <div className="z-10 max-w-screen-lg px-4 lg:px-0">
           <div className="flex flex-col items-center gap-y-6 rounded-xl bg-gray-800/60 p-6 text-center shadow-lg backdrop-blur-sm">
             <h1
@@ -27,8 +31,8 @@ const Hero: FC = memo(() => {
               style={{
                 color: 'transparent',
                 WebkitTextStroke: '0.5px white' /* WebKit browsers (Safari, Chrome) */,
-                // textStroke: '0.5px white' /* Standard */,
-              }}>
+              }}
+            >
               {secondaryName}
             </h1>
             <h1 className="text-4xl font-bold text-white sm:text-5xl lg:text-7xl relative">{name}</h1>
@@ -38,7 +42,7 @@ const Hero: FC = memo(() => {
               <Socials />
             </div>
             <div className="flex w-full justify-center gap-x-4">
-              {actions.map(({href, text, primary, Icon}) => (
+              {actions.map(({ href, text, primary, Icon }) => (
                 <a
                   className={classNames(
                     'flex gap-x-2 rounded-full border-2 bg-none px-4 py-2 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
@@ -59,7 +63,8 @@ const Hero: FC = memo(() => {
         <div className="absolute inset-x-0 bottom-6 flex justify-center">
           <a
             className="rounded-full bg-white p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
-            href={`/#${SectionId.About}`}>
+            href={`/#${SectionId.About}`}
+          >
             <ChevronDownIcon className="h-5 w-5 bg-transparent sm:h-6 sm:w-6" />
           </a>
         </div>
